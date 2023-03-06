9f86cf9c9eea23ef9feb1ef950803b1e980526b4
sudo-lupus <67869327+sudo-lupus@users.noreply.github.com>
2023-03-06 16:19:03 +0100

    Add Log Files for Testing

--- test.py
+++ test.py
@@ -3,12 +3,16 @@ import config
 import markdown
 import datetime
 
+messages = []
+
 def request(prompt, model = "gpt-3.5-turbo", api_key = config.api_key):
+    global messages
+    messages.append({"role": "user", "content": prompt})
     prompt += "\nFormulate your whole answer in Markdown Format"
     openai.api_key = api_key
     completion = openai.ChatCompletion.create(
-      model= model, 
-      messages=[{"role": "user", "content": prompt}]
+      model= model,
+      messages = messages
     )
     message = str.strip(completion["choices"][0]["message"]["content"])
     return message
@@ -17,21 +21,16 @@ counter = 0
 chat_log = []
 file_path = ""
 
-markdown.write_to_md_file(f"\# Session of {datetime.datetime.now()}\n  ", file_path= file_path)
+file_path = markdown.create_initial_md_file(overwrite=True)
+markdown.write_to_md_file(f"\n\# Session of {datetime.datetime.now()}\n  ", file_path= file_path)
 
 while(True):
     prompt = input("What would you like to ask ChatGPT?\nEnter 'Q' to exit\nYou: ")
     if(prompt in ["Q", "q"]):
         print("You have exited the chat")
         break
-    if(prompt == "n"):
-        file_path = markdown.create_initial_md_file(overwrite=True)
-        continue
     message = request(prompt)
+    messages.append(message)
     print(f"Current file path = {file_path}")
-
     print(f"\nChatGPT: {message}\n")
     markdown.write_to_md_file(f'''  \n### Question: {prompt}\n### Answer:\n{message}''', file_path = file_path)
-
-for i, msg in enumerate(chat_log):
-    markdown.write_to_md_file(f'''### {i + 1} Question: {msg[0]}  \n### Answer: \n{msg[1]}''', file_path= file_path)
